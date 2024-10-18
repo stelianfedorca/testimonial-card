76ad717836e0ee75c038947324af7e1eb928b037
Stelian Fedorca <stelian.fedorca25@gmail.com>
2024-10-19 00:37:37 +0300

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    base: './testimonial-card',
-    // Other build configuration options...
-  },
+  base: './testimonial-card',
+  // Other build configuration options...
 });
