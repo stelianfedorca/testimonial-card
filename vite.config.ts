344eda7762a90d5e432d75548b896cb061cafba7
Stelian Fedorca <stelian.fedorca25@gmail.com>
2024-10-19 00:39:47 +0300

    added trailing slash

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './testimonial-card',
+  base: './testimonial-card/',
   // Other build configuration options...
 });
